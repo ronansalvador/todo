3b909d1cd62fc86a3c76345eff0f72bd14ea3fbf
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-12-27 10:12:51 -0300

    feat: add counter taks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,26 @@ import { useToDo } from './context/ToDoContext'
 
 const Home = () => {
   const { todos } = useToDo()
+  const done = todos.filter((todo) => todo.completed === true)
   return (
     <main className={`${styles.main}`}>
       <h1>Lista de Tarefas</h1>
       <TodoForm />
-      {todos.length > 0 ? <TodoList /> : <p>Lista de Tarefas Vazia!</p>}
+      {todos.length > 0 ? (
+        <>
+          <div className={styles.count_tasks}>
+            <p>
+              Tarefas criadas: <span>{todos.length}</span>
+            </p>
+            <p>
+              Concluidas: <span>{`${done.length} de ${todos.length}`}</span>
+            </p>
+          </div>
+          <TodoList />
+        </>
+      ) : (
+        <p>Lista de Tarefas Vazia!</p>
+      )}
     </main>
   )
 }
